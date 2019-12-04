68f7cbfd53eaaadaf7965bcc329911a690a33793
VK <vitaliy.krynytskyy@gmail.com>
2019-12-04 11:03:39 +0000

    Updated the styling of the profile page and added a home button

--- pages/Profile/index.js
+++ pages/Profile/index.js
@@ -1,13 +1,36 @@
 import React from "react";
 import withAuth from "../../src/helpers/withAuth";
+
+import { Container, Typography, Box, Button } from "@material-ui/core";
+
 import Navbar from "../../src/components/Navbar";
+import Link from "../../src/components/Link";
+import Copyright from "../../src/components/Copyright";
 
 const Profile = props => {
   const { currentUser } = props;
   return (
     <React.Fragment>
       <Navbar pageTitle={"Profile"} currentUser={currentUser} />
-      <p>this page is auth protected</p>
+
+      <Container maxWidth="xl">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            This is an auth protected page
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            naked
+            href="/"
+          >
+            Home
+          </Button>
+
+          <Copyright />
+        </Box>
+      </Container>
     </React.Fragment>
   );
 };
