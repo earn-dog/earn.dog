6ec702332ea99a005a8a800089a9ae0e002c214b
VK <vitaliy.krynytskyy@gmail.com>
2019-12-05 12:06:44 +0000

    Added environment variables for ZEIT now hosting

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,25 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: Buffer.from(process.env.FIREBASE_API_KEY, "base64").toString(),
+  authDomain: Buffer.from(
+    process.env.FIREBASE_AUTH_DOMAIN,
+    "base64"
+  ).toString(),
+  databaseURL: Buffer.from(
+    process.env.FIREBASE_DATABASE_URL,
+    "base64"
+  ).toString(),
+  projectId: Buffer.from(process.env.FIREBASE_PROJECT_ID, "base64").toString(),
+  storageBucket: Buffer.from(
+    process.env.FIREBASE_STORAGE_BUCKET,
+    "base64"
+  ).toString(),
+  messagingSenderId: Buffer.from(
+    process.env.FIREBASE_MESSAGING_SENDER_ID,
+    "base64"
+  ).toString(),
+  appId: Buffer.from(process.env.FIREBASE_APP_ID, "base64").toString()
 };
 
 if (!firebase.apps.length) {
