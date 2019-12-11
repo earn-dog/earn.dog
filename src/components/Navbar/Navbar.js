f5279bb46e0fb51a07ffd93a5645f26d6135af6e
VK <vitaliy.krynytskyy@gmail.com>
2019-12-11 09:06:05 +0000

    Changed profile icon button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,30 +6,12 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  IconButton,
-  MenuItem,
-  Menu
 } from "@material-ui/core";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 
-import { auth } from "../../firebase/firebase";
 import * as routes from "../../constants/routes";
+import ProfileIcon from "../ProfileIcon";
 
 const Navbar = ({ pageTitle, parentClasses, authUser }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleMenu = e => {
-    setAnchorEl(e.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleGoToProfilePage = () => {
-    Router.push(routes.PROFILE);
-  };
 
   const handleGoToHomePage = () => {
     Router.push(routes.HOME);
@@ -46,37 +28,7 @@ const Navbar = ({ pageTitle, parentClasses, authUser }) => {
           >
             {pageTitle}
           </Typography>
-          {authUser && (
-            <>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleGoToProfilePage}>Profile</MenuItem>
-                <MenuItem onClick={() => auth.signOut()}>Sign Out</MenuItem>
-              </Menu>
-            </>
-          )}
+          {authUser && <ProfileIcon />}
         </Toolbar>
       </AppBar>
     </React.Fragment>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Shell } from "./Shell";
 export { default as Navbar } from "./Navbar";
 export { default as Card } from "./Card";
+export { default as ProfileIcon } from "./ProfileIcon";
