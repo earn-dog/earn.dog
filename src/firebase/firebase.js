044431a5b29032a3a9b88fabc363a423bee3a28a
VK <vitaliy.krynytskyy@gmail.com>
2019-12-12 13:52:47 +0000

    Added deployment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,24 +3,62 @@ import "firebase/auth";
 import "firebase/database";
 
 const prodConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: Buffer.from(process.env.FIREBASE_API_KEY, "base64").toString(),
+  authDomain: Buffer.from(
+    process.env.FIREBASE_AUTH_DOMAIN,
+    "base64"
+  ).toString(),
+  databaseURL: Buffer.from(
+    process.env.FIREBASE_DATABASE_URL,
+    "base64"
+  ).toString(),
+  projectId: Buffer.from(
+    process.env.FIREBASE_DATABASE_URL,
+    "base64"
+  ).toString(),
+  storageBucket: Buffer.from(
+    process.env.FIREBASE_PROJECT_ID,
+    "base64"
+  ).toString(),
+  messagingSenderId: Buffer.from(
+    process.env.FIREBASE_STORAGE_BUCKET,
+    "base64"
+  ).toString(),
+  appId: Buffer.from(process.env.FIREBASE_APP_ID, "base64").toString()
 };
 
 const devConfig = {
-  apiKey: process.env.FIREBASE_API_KEY_DEV,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN_DEV,
-  databaseURL: process.env.FIREBASE_DATABASE_URL_DEV,
-  projectId: process.env.FIREBASE_PROJECT_ID_DEV,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET_DEV,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_DEV,
-  appId: process.env.FIREBASE_APP_ID_DEV
+  apiKey: Buffer.from(process.env.FIREBASE_API_KEY_DEV, "base64").toString(),
+  authDomain: Buffer.from(
+    process.env.FIREBASE_AUTH_DOMAIN_DEV,
+    "base64"
+  ).toString(),
+  databaseURL: Buffer.from(
+    process.env.FIREBASE_DATABASE_URL_DEV,
+    "base64"
+  ).toString(),
+  projectId: Buffer.from(
+    process.env.FIREBASE_PROJECT_ID_DEV,
+    "base64"
+  ).toString(),
+  storageBucket: Buffer.from(
+    process.env.FIREBASE_STORAGE_BUCKET_DEV,
+    "base64"
+  ).toString(),
+  messagingSenderId: Buffer.from(
+    process.env.FIREBASE_MESSAGING_SENDER_ID_DEV,
+    "base64"
+  ).toString(),
+  appId: Buffer.from(process.env.FIREBASE_APP_ID_DEV, "base64").toString()
 };
+
+// FIREBASE_API_KEY_DEV
+// FIREBASE_AUTH_DOMAIN_DEV
+// FIREBASE_DATABASE_URL_DEV
+// FIREBASE_PROJECT_ID_DEV
+// FIREBASE_STORAGE_BUCKET_DEV
+// FIREBASE_MESSAGING_SENDER_ID_DEV
+// FIREBASE_APP_ID_DEV
 
 const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
 
